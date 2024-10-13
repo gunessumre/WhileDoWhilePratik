5367b31566cd95bfcb74633ea6edb1180b975181
gunessumre <73123396+gunessumre@users.noreply.github.com>
2024-10-13 16:18:23 +0300

    Add files via upload

---
+++ WhileDowhilePratik/WhileDowhilePratik/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//WHILE ILE YAPIM
+int limit;
+Console.Write("Bir limit değeri giriniz: ");
+limit = int.Parse(Console.ReadLine());
+int sayac = 0;
+while (sayac <= limit)
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+}
+
+//DO WHILE ILE YAPIM
+int limit1;
+Console.Write("Bir limit değeri giriniz: ");
+limit1 = int.Parse(Console.ReadLine());
+int sayac1 = 0;
+do
+{
+    Console.WriteLine("Ben bir Patika'lıyım");
+    sayac++;
+} while (sayac<= limit1);
